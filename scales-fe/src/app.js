332ed4ac5ba59e001a80d044929be795621c7578
ekeith <evanmkeith@gmail.com>
2022-05-07 13:40:08 -0700

    setup routing

--- scales-fe/src/app.js
+++ scales-fe/src/app.js
@@ -1,7 +1,11 @@
-import React, { useEffect,  createContext, useState } from 'react';
-import Login from '../../components/Login'; 
-import { Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
+import Profile from './pages/Profile'; 
+import Artist from './pages/Artist'; 
+import Listen from './pages/Listen'; 
+import Playlist from './pages/Playlist'; 
+import Search from './pages/Search'; 
+import TourInfo from './pages/TourInfo'; 
 
 function App() {
 
@@ -13,9 +17,29 @@ function App() {
                 element={<Landing />}
             ></Route>
             <Route
-                path="/"
-                element={<Landing />}
+                path="/profile"
+                element={<Profile />}
             ></Route>
+            <Route
+                path="/artist"
+                element={<Artist />}
+            ></Route>
+            <Route
+                path="/listen"
+                element={<Listen />}
+            ></Route>
+            <Route
+                path="/playlist"
+                element={<Playlist />}
+            ></Route>
+            <Route
+                path="/search"
+                element={<Search />}
+            ></Route>
+            <Route 
+                path="/touring_info"
+                element={<TourInfo />}
+            ></Route>   
         </Routes>
     </>
   );
