7910634d1f9b216e6a817cf4a5506323c1515b51
ekeith <evanmkeith@gmail.com>
2022-05-11 18:05:28 -0700

    put button and fucntion on playlisty page

--- scales-fe/src/pages/Playlist/index.js
+++ scales-fe/src/pages/Playlist/index.js
@@ -1,8 +1,23 @@
+import React, { useContext } from 'react';
+import * as spotifyAuthService from '../../api/spotify.auth';
+import { UserContext } from '../../UserContext';
 
 export default function Playlist() {
+    const { user, setUser } = useContext(UserContext);
+
+    const createSpotifyPlaylist = async() => {
+        await spotifyAuthService.createSpotifyPlaylist(user.userId).then(
+            (res) => {
+                console.log("Sent ", user.userId);
+                console.log(res);
+            }
+        )
+    } 
+
     return (
         <>
             <h1>Playlist</h1>
+            <button onClick={createSpotifyPlaylist}>Create Playlist</button>
         </>
     )
 }
--- scales-fe/src/pages/Profile/index.js
+++ scales-fe/src/pages/Profile/index.js
@@ -3,21 +3,10 @@ import * as spotifyAuthService from '../../api/spotify.auth';
 import { UserContext } from '../../UserContext';
 
 export default function Profile() {
-    const { user, setUser } = useContext(UserContext);
-
-    const createSpotifyPlaylist = async() => {
-        await spotifyAuthService.createSpotifyPlaylist(user.userId).then(
-            (res) => {
-                console.log("Sent ", user.userId);
-                console.log(res);
-            }
-        )
-    } 
 
     return (
         <>
             <h1>Profile</h1>
-            <button onClick={createSpotifyPlaylist}>Create Playlist</button>
         </>
     )
 }
