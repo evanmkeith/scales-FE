072c4a7db881b3e5c6e7d32bcb9cab3b06f9d1c6
ekeith <evanmkeith@gmail.com>
2022-05-14 12:06:29 -0700

    setup edit profile

--- scales-fe/src/api/user.service.js
+++ scales-fe/src/api/user.service.js
@@ -4,7 +4,21 @@ const getUserInfo = (id) => {
     return axios.post('http://localhost:4000/api/user/info', {id});
 }
 
+const editProfile = (id, body) => {
+    return axios.post('http://localhost:4000/api/user/edit', {id, body});
+}
+
+const addArtist = (id, artist) => {
+    return axios.post('http://localhost:4000/api/user/add_artist', {id, artist});
+}
+
+const destroyProfile = (id) => {
+    return axios.post('http://localhost:4000/api/user/destroy', {id});
+}   
 
 export {
-    getUserInfo
+    getUserInfo, 
+    editProfile, 
+    addArtist, 
+    destroyProfile
 }
--- scales-fe/src/pages/Profile/index.js
+++ scales-fe/src/pages/Profile/index.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import * as userService from '../../api/user.service';
 import { UserContext } from '../../UserContext';
 
 export default function Profile() {
     const { user, setUser } = useContext(UserContext);
     const [ userProfile, setUserProfile ] = useState({});
+    const [ edit, setEdit ] = useState(false);
+    const [ image, setImage ] = useState(); 
+    const [ name, setName ] = useState();
+    const navigate = useNavigate();
 
     const getUser = async() => {
         const id = user.userId;
@@ -14,19 +19,54 @@ export default function Profile() {
         })
     }
 
+    const editProfile = () => {
+        setEdit(true);
+    }
+
+    const updateProfile = async(e) => {
+        e.preventDefault();
+        const id = user.userId;
+        const body = {
+            name: name,
+            img: image
+        }
+        await userService.editProfile(id, body).then((res) => {
+            setUserProfile(res.data.updatedUser)
+            setEdit(false);
+        })
+    }
+
     useEffect(() => {
         getUser()
     }, []);
 
-    return (
-        <>
-            <h1>Profile</h1>
-            <div> 
-                {!userProfile.img  ? (<img src={userProfile.img} />) : (<img src="https://picsum.photos/200/200?grayscale" />)} 
-            </div> 
-            <p>{userProfile.name}</p>
-            <p>Artists I want to see live</p>
-            <p>{userProfile.artists}</p>
-        </> 
-    )
+    if(!edit){
+        return (
+            <>
+                <div> 
+                    <img src={userProfile.img} />
+                </div> 
+                <p>{userProfile.name}</p>
+                <p>Artists I want to see live</p>
+                <p>{userProfile.artists}</p>
+                <button onClick={editProfile}>Edit</button>
+            </> 
+        )
+    } else {
+        return (
+            <>
+                <form>
+                    <label>
+                        Image
+                    <input onChange={e => setImage(e.target.value)} value={image} placeholder={userProfile.img}/>
+                    </label>
+                    <label>
+                        Name
+                    <input onChange={e => setName(e.target.value)} value={name} placeholder={userProfile.name}/>
+                    </label>
+                    <button onClick={updateProfile}>Submit</button>
+                </form>
+            </> 
+        )
+    }
 }
