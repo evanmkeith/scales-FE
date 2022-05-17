1dc921ab331acb069772720f40c90e797c76c0c8
ekeith <evanmkeith@gmail.com>
2022-05-16 19:51:51 -0700

    fixed landing

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -11,6 +11,9 @@ function Landing() {
         <img src={logo}/>
         <span>A place to enjoy music by the album. <br/>Create a playlist from songs you like, that you can see on your Spotify account, <br/>and check to see if artists you admire are on tour.<br/><br/>Enjoy music, slowly.</span>
       </div>
+      <div>
+        
+      </div>
     </div>
   );
 }
